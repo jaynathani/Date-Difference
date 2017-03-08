ed1d135a3815011ddcd280912b45153951366d80
Jay Nathani <jaynathani@gmail.com>
2017-03-07 23:27:19 -0600

    Input two dates from user.

--- src/DD.java
+++ src/DD.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class DD {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter date 1(dd-mm-yyyy): ");
+		String str1 = reader.next();
+		System.out.println("Enter date 2(dd-mm-yyyy): ");
+		String str2 = reader.next();
+		System.out.println("You entered: " + str1 + " and " + str2);
 	}
 
 }
